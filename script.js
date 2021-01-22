cbceb5a5a2133933e44bb2f7e806792d47d7ee81
AubreyBennett <27aubreyb@gmail.com>
2021-01-21 21:20:25 -0500

    adding additional variables

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var apiKey = ekSU7YWoghGMsmMBcqsinm9MNpHIbC3E
 var queryURL = "http://developer.nytimes.com/article_search_v2.json" + apiKey;
 
+var searchTerm = "";
+var numberRecords = 0;
+var startYear = 0;
+var endYear = 0;
+
 $.ajax({
     url: queryURL,
     method: "GET"
